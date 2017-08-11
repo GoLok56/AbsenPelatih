5c0aa1afa490ed62272434b743c53e0a6f12de7c
GoLok56 <satadii11@gmail.com>
2017-08-11 09:06:35 +0700

    add main activity view

---
+++ app/src/main/java/io/github/golok56/view/IMainActivityView.java
@@ -0,0 +1,13 @@
+package io.github.golok56.view;
+
+import io.github.golok56.view.base.IBaseView;
+
+/**
+ * Interface that {@link io.github.golok56.absenpelatih.MainActivity} need to implement. So it can
+ * interact with the corresponding presenter.
+ *
+ * @author Satria Adi Putra
+ */
+public interface IMainActivityView extends IBaseView {
+
+}
