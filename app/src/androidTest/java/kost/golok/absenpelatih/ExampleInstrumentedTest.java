256660fcd91cc85081e8e24a0ee4bc7931c628ff
GoLok56 <satadii11@gmail.com>
2017-08-11 08:43:49 +0700

    minor change on adapter

--- app/src/androidTest/java/kost/golok/absenpelatih/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/kost/golok/absenpelatih/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("kost.golok.absenpelatih", appContext.getPackageName());
+        assertEquals("kost.io.github.golok56.absenpelatih", appContext.getPackageName());
     }
 }
