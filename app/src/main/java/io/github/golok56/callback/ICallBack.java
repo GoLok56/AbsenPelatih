12575656d73a14a285e8352fcd7a425e6d6aadc0
GoLok56 <satadii11@gmail.com>
2017-08-11 09:51:47 +0700

    add callback interface

---
+++ app/src/main/java/io/github/golok56/callback/ICallBack.java
@@ -0,0 +1,19 @@
+package io.github.golok56.callback;
+
+/**
+ * Interface to do a callback when operation from database is success.
+ *
+ * @author Satria Adi Putra
+ */
+public interface ICallBack {
+
+    /**
+     * A callback when the operation is a success.
+     */
+    void onSuccess();
+
+    /**
+     * A callback if the operation is failed.
+     */
+    void onFailed();
+}
