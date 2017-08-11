edc5e34a77179702ebbecca411fd6ecc252c554c
GoLok56 <satadii11@gmail.com>
2017-08-11 10:57:03 +0700

    fix import

--- app/src/main/java/io/github/golok56/callback/IOnInsertCompleted.java
+++ app/src/main/java/io/github/golok56/callback/IOnInsertCompleted.java
@@ -1,11 +1,12 @@
 package io.github.golok56.callback;
 
+import io.github.golok56.callback.base.IBaseOnOperationCompleted;
+
 /**
  * Interface to do a callback when insert operation from database is success.
  *
  * @author Satria Adi Putra
  */
-
 public interface IOnInsertCompleted extends IBaseOnOperationCompleted {
 
     /**
